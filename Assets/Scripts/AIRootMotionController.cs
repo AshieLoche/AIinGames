2d8bb125f1fd693cd1f726dfae35a3c5be89868e
CIITSHSDTP00\CIIT <asherstar1001@gmail.com>
2025-03-12 20:36:07 +0800

    Week 9

--- Assets/Scripts/AIRootMotionController.cs
+++ Assets/Scripts/AIRootMotionController.cs
@@ -1,11 +1,15 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class AIRootMotionController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private Animator _animator;
     [SerializeField] private float _maxTarget;
+
+    private bool _isOffMesh;
 
     private void OnValidate()
     {
@@ -17,17 +21,30 @@ public class AIRootMotionController : MonoBehaviour
     {
         if (_navMeshAgent.hasPath)
         {
-            var dir = (_navMeshAgent.steeringTarget - transform.position).normalized;
-            var aniDir = transform.InverseTransformDirection(dir);
-            var isFacingMoveDirection = Vector3.Dot(dir, transform.forward) > .5f;
-            
-            _animator.SetFloat("Horizontal", isFacingMoveDirection ? aniDir.x : 0, 0.5f, Time.deltaTime);
-            _animator.SetFloat("Vertical", isFacingMoveDirection ? aniDir.z : 0, 0.5f, Time.deltaTime);
+            if (_navMeshAgent.isOnOffMeshLink)
+            {
+                if (!_isOffMesh)
+                {
+                    _isOffMesh = true;
+                    var link = _navMeshAgent.currentOffMeshLinkData;
+                    StartCoroutine(DoOffMeshLink(link));
+                }
+                else 
+                {
+                    _isOffMesh = false;
+                    var dir = (_navMeshAgent.steeringTarget - transform.position).normalized;
+                    var aniDir = transform.InverseTransformDirection(dir);
+                    var isFacingMoveDirection = Vector3.Dot(dir, transform.forward) > .5f;
+                    
+                    _animator.SetFloat("Horizontal", isFacingMoveDirection ? aniDir.x : 0, 0.5f, Time.deltaTime);
+                    _animator.SetFloat("Vertical", isFacingMoveDirection ? aniDir.z : 0, 0.5f, Time.deltaTime);
 
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir), 100 * Time.deltaTime);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir), 100 * Time.deltaTime);
 
-            if (Vector3.Distance(transform.position, _navMeshAgent.destination) < _navMeshAgent.radius)
-                _navMeshAgent.ResetPath();
+                    if (Vector3.Distance(transform.position, _navMeshAgent.destination) < _navMeshAgent.radius)
+                        _navMeshAgent.ResetPath();
+                }
+            }
         }
         else
         {
@@ -54,5 +71,41 @@ public class AIRootMotionController : MonoBehaviour
                 Debug.DrawLine(_navMeshAgent.path.corners[i], _navMeshAgent.path.corners[i + 1], Color.red);
             }
         }
+    }
+
+    private IEnumerator DoOffMeshLink(OffMeshLinkData link)
+    {
+        while (true)
+        {
+            transform.position = Vector3.Lerp(transform.position, link.endPos, Time.deltaTime);
+            var dir = (link.endPos - link.startPos).normalized;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir), 100 * Time.deltaTime);
+
+            var isRotationGood = Vector3.Dot(dir, transform.forward) > .8f;
+
+            if (isRotationGood)
+                break;
+            
+            yield return new WaitForSeconds(0);
+        }
+
+        _animator.CrossFade("Jump", 0.2f);
+
+        yield return new WaitForSeconds(0.2f);
+        
+        var time = 1f;
+        var totalTime = time;
+
+        while (time > 0)
+        {
+            time = Mathf.Max(0, time - Time.deltaTime);
+            var goal = Vector3.Lerp(link.startPos, link.endPos, 1 - time / totalTime);
+            var elapsedTime = totalTime - time;
+            transform.position= elapsedTime > .3f ? goal : Vector3.Lerp(transform.position, goal, elapsedTime / .3f);
+            yield return new WaitForSeconds(0);
+        }
+
+        transform.position = link.endPos;
+        _navMeshAgent.CompleteOffMeshLink();
     }
 }
